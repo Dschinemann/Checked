d5eb57dccae283dce1f1abedc0dfa9899077fae4
Diego Schinemann <ti03@contlog.com.br>
2022-08-13 11:53:50 -0300

    adicionado campo complementar para ocorrencias

---
+++ Checked/Models/Models/Complement/OccurrenceComplement.cs
@@ -0,0 +1,15 @@
+﻿namespace Checked.Models.Models.Complement
+{
+    public class OccurrenceComplement
+    {
+        public Occurrence Occurrence { get; set; }
+        public string OccurrenceId { get; set; }
+        public Organization Organization { get; set; }
+        public string OrganizationId { get; set; }
+
+        /***/
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string ComplementTitle { get; set; }
+        public string ComplementDescription { get; set; }
+    }
+}
